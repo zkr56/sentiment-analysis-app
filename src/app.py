d3f421e562b9d761532d76828202025a91d481f7
zkr56 <aboudiakiteaz12@gmail.com>
2025-04-22 09:42:58 +0000

    Fix: correction des chemins vers les modèles

--- src/app.py
+++ src/app.py
@@ -4,13 +4,13 @@ import pickle
 from utils.text_processing import clean_text
 
 # Chargement du modèle, du vectorizer et de l'encodeur de labels
-with open('models/sentiment_model.pkl', 'rb') as model_file:
+with open('src/models/sentiment_model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
-with open('models/vectorizer.pkl', 'rb') as vectorizer_file:
+with open('src/models/vectorizer.pkl', 'rb') as vectorizer_file:
     vectorizer = pickle.load(vectorizer_file)
 
-with open('models/label_encoder.pkl', 'rb') as label_encoder_file:
+with open('src/models/label_encoder.pkl', 'rb') as label_encoder_file:
     label_encoder = pickle.load(label_encoder_file)
 
 # Titre de l'application
